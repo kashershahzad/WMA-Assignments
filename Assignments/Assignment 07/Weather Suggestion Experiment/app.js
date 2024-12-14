4ff84ee41a01cbac5c44bbfd4496890de0e499f3
kashershahzad <kashershahzadprogramming@gmail.com>
2024-12-14 14:53:23 +0500

    update

---
+++ Assignments/Assignment 07/Weather Suggestion Experiment/app.js
@@ -0,0 +1,15 @@
+var userInput;
+userInput = prompt(" Enter weather (Sunny, Rainy, Cold) ")
+
+{
+    if (userInput === "Sunny") {
+        console.log("Wear sunglasses")
+    }
+    else if (userInput === "Rainy") {
+        console.log("Take an umbrella" )
+    }
+   else if (userInput === "Cold") {
+        console.log("Wear a jacket")
+    }
+   
+}
