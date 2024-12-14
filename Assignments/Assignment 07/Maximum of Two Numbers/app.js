29faf4e425ed08897f25743d872846f978906429
kashershahzad <kashershahzadprogramming@gmail.com>
2024-12-14 15:32:04 +0500

    added

---
+++ Assignments/Assignment 07/Maximum of Two Numbers/app.js
@@ -0,0 +1,14 @@
+
+
+var userInput1 = prompt("Please enter first input")
+var userInput2 = prompt("Please enter second input")
+
+{
+    if (userInput1>userInput2) {
+        console.log(`${userInput1} is grester`)
+    }
+    else if (userInput1<userInput2) {
+        console.log(`${userInput2} is grester`)
+    }
+   
+}
