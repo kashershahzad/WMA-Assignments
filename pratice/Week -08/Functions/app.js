51f044a6c225098f49d0458f9978a871e560fe05
kashershahzad <kashershahzadprogramming@gmail.com>
2024-12-20 15:16:26 +0500

    UP

---
+++ pratice/Week -08/Functions/app.js
@@ -0,0 +1,23 @@
+// FUNCTIONS 
+// TWO TYPES OF FUCTIONS
+// BUILT IN FUNCTION
+
+// EXAMPLE
+// .log() is a builtin fuction
+
+
+// USER-DEFINE FUNCTION
+
+
+// EXAMPLE
+// function abc() { console.log("line 1") };
+// console.log("line 2")
+// abc();
+// console.log("line 3")
+// abc();
+// console.log("line 4")
+// abc();
+// console.log("line 5")
+// abc();
+
+// USER-DEFINE FUNCTIONS
