b804fbc2efcd606407d0768c38acddded8987b7f
kashershahzad <kashershahzadprogramming@gmail.com>
2024-12-20 13:49:52 +0500

    loop

---
+++ pratice/Week -08/Loops/app.js
@@ -0,0 +1,23 @@
+// While Loop
+
+let i = 0;
+
+while (i < 10) {
+    console.log("while", i);
+    i = i + 1;
+}
+
+
+// Do while Loop
+
+let a = 0;
+do {
+    console.log("do-while ", a)
+    a = a + 1;
+} while (a < 10);
+
+// For Loop 
+
+for (let b = 0; b < 10; b++) {
+    console.log("For", b)
+}
