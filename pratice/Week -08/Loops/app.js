fe167ad9168a61918155ebce53f9b51e10e741e7
kashershahzad <kashershahzadprogramming@gmail.com>
2024-12-20 14:21:36 +0500

    program

--- pratice/Week -08/Loops/app.js
+++ pratice/Week -08/Loops/app.js
@@ -1,24 +1,35 @@
 // While Loop
 
-let i = 0;
+// let i = 0;
 
-while (i < 10) {
-    console.log("while", i);
-    i = i + 1;
-}
+// while (i < 10) {
+//     console.log("while", i);
+//     i = i + 1;
+// }
 
 
 // Do while Loop
 // It always  runs atleast 1 time then check the condition
 
-let a = 0;
-do {
-    console.log("do-while ", a)
-    a = a + 1;
-} while (a < 10);
+// let a = 0;
+// do {
+//     console.log("do-while ", a)
+//     a = a + 1;
+// } while (a < 10);
 
 // For Loop 
 
-for (let b = 0; b < 10; b++) {
-    console.log("For", b)
-}
+// for (let b = 0; b < 10; b++) {
+//     console.log("For", b)
+// }
+
+// Some of even number between 50 t0 100 
+// let sum = 0;
+// for (i = 0; i <= 100; i++) {
+//     if (i % 2 == 0 && i>50) {
+//         sum = sum + i;
+//     }
+// }
+
+// console.log(sum);
+
